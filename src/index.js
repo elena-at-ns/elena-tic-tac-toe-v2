cb143ecf04c74c64243c9e9aebeb38c3f01c2bee
Elena Popa <elena.popa@ns.nl>
2025-01-24 11:53:31 +0100

    3/allow X to win by completing the first row

--- src/index.js
+++ src/index.js
@@ -5,12 +5,26 @@ class TicTacToe {
       ["", "", ""],
       ["", "", ""],
     ];
+
+    this.currentPlayer = "X";
   }
 
   playTurn(x, y, mark) {
     if (this.board[x][y] === "") {
       this.board[x][y] = mark;
     }
+  }
+
+  checkWinner() {
+    if (
+      this.board[0][0] === this.board[0][1] &&
+      this.board[0][1] === this.board[0][2] &&
+      this.board[0][0] !== ""
+    ) {
+      return "X";
+    }
+
+    return null;
   }
 }
 
--- tests/index.test.js
+++ tests/index.test.js
@@ -15,4 +15,14 @@ describe("Index.js", () => {
     game.playTurn(0, 0, "X");
     expect(game.board[0][0]).toBe("X");
   });
+
+  test("first player X wins by completing the first row", () => {
+    const game = new TicTacToe();
+    game.playTurn(0, 0, "X");
+    game.playTurn(1, 0, "O");
+    game.playTurn(0, 1, "X");
+    game.playTurn(2, 0, "O");
+    game.playTurn(0, 2, "X");
+    expect(game.checkWinner()).toBe("X");
+  });
 });
